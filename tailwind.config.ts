615240291c53596a6612df51febcc8fafef44c52
liwei <liwei-8466@qq.com>
2024-03-04 22:16:02 +0800

    feat: change to nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
-import daisyui from "daisyui"
+import {nextui} from "@nextui-org/react";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./src/**/*.tsx",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -11,5 +14,6 @@ export default {
       },
     },
   },
-  plugins: [daisyui],
+  darkMode: "class",
+  plugins: [nextui()]
 } satisfies Config;
