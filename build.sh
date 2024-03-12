0560451d00e51e617584a2634541e7eebb2e6a66
liwei <liwei-8466@qq.com>
2024-03-12 15:21:38 +0800

    fix: production account

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [ "$1" = "frontend" ]; then
   echo "Building the frontend"
   cat <<EOF >.env
 VITE_APP_URL="https://api.dot-20.xyz/trpc"
-VITE_MARKET_ACCOUNT="14rLmTokV8HRPaxgkN7jtLzGKP1T7R2hoK5GNGmqtgCyJNYA"
+VITE_MARKET_ACCOUNT="12TXMUbBPt2N7SR6phqxUY6cRv8rJrGdwXpAotfELuR1UUDi"
 VITE_POLKADOT_SCAN="https://polkadot.subscan.io"
 VITE_POLKADOT_ENDPOINT="wss://rpc.polkadot.io"
 VITE_POLKADOT_DECIMALS=10
