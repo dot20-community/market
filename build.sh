f7dc95f34edc4bff38b9fff83348dcd973c21930
liwei <liwei-8466@qq.com>
2024-03-12 15:15:50 +0800

    feat: 前端主网配置

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ if [ "$1" = "frontend" ]; then
   echo "Building the frontend"
   cat <<EOF >.env
 VITE_APP_URL="https://api.dot-20.xyz/trpc"
-VITE_MARKET_ACCOUNT="12eUnt8hcwtmcVgShgm4YuRYcH448tgj9qMDJ5r9tTJisdpe"
-VITE_POLKADOT_SCAN="https://westend.subscan.io"
-VITE_POLKADOT_ENDPOINT="wss://westend-rpc.polkadot.io"
-VITE_POLKADOT_DECIMALS=12
+VITE_MARKET_ACCOUNT="14rLmTokV8HRPaxgkN7jtLzGKP1T7R2hoK5GNGmqtgCyJNYA"
+VITE_POLKADOT_SCAN="https://polkadot.subscan.io"
+VITE_POLKADOT_ENDPOINT="wss://rpc.polkadot.io"
+VITE_POLKADOT_DECIMALS=10
 VITE_MIN_SELL_TOTAL_PRICE=2
 VITE_SERVER_FEE_RATE=0.02
 EOF
