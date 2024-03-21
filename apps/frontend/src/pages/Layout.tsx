2d06e7d6e457bf9103263e37cc0d871dd36ac58d
kenny <kenny@tiancai.pro>
2024-03-21 10:53:35 +0800

    停机维护通知

--- apps/frontend/src/pages/Layout.tsx
+++ apps/frontend/src/pages/Layout.tsx
@@ -206,7 +206,10 @@ export function Layout() {
           </NavbarItem>
         </NavbarContent>
       </Navbar>
-
+      <div className="py-1 w-full bg-stone-600 flex justify-center ">
+        The official index is being upgraded and maintained, please wait
+        patiently.
+      </div>
       <main className="flex-grow">
         <Outlet />
       </main>
