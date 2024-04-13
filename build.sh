b713fc5593c2665ad24629d4b7a0d3497799e79d
liwei <liwei-8466@qq.com>
2024-04-13 18:06:59 +0800

    fix: scan host

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if [ "$1" = "frontend" ]; then
   cat <<EOF >.env
 VITE_APP_URL="https://api.dot-20.xyz/trpc"
 VITE_MARKET_ACCOUNT="12TXMUbBPt2N7SR6phqxUY6cRv8rJrGdwXpAotfELuR1UUDi"
-VITE_POLKADOT_SCAN="https://polkadot.subscan.io"
+VITE_POLKADOT_SCAN="https://assethub-polkadot.subscan.io"
 VITE_POLKADOT_ENDPOINT="wss://rpc.polkadot.io"
 VITE_ASSET_HUB_ENDPOINT="wss://polkadot-asset-hub-rpc.polkadot.io"
 VITE_POLKADOT_DECIMALS=10
